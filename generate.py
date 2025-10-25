27294cb878e2c1c79380364742f614439b741fa1
Birat Poudel <biratpoudel@lftechnology.com>
2025-10-25 21:48:35 +0545

    Adjust positions in generate.py, and enhance simulation control in simulate.py

--- generate.py
+++ generate.py
@@ -10,17 +10,15 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
 
-    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1.0], size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[1, 1, 1])
 
-    # Back leg (slightly behind, Y = -0.5)
     pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg",
-                       type="revolute", position=[0, -0.5, 0.51])
-    pyrosim.Send_Cube(name="BackLeg", pos=[0, -0.5, -0.5], size=[1, 1, 1])
+                       type="revolute", position=[1, 0, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
 
-    # Front leg (slightly in front, Y = +0.5)
     pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg",
-                       type="revolute", position=[0, 0.5, 0.51])
-    pyrosim.Send_Cube(name="FrontLeg", pos=[0, 0.5, -0.5], size=[1, 1, 1])
+                       type="revolute", position=[2, 0, 1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[1, 1, 1])
 
     pyrosim.End()
 
--- simulate.py
+++ simulate.py
@@ -23,11 +23,38 @@ pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = np.zeros(1000)
 frontLegSensorValues = np.zeros(1000)
 
+BackLegAmplitude = np.pi/6
+BackLegFrequency = 2.0
+BackLegPhaseOffset = 0
+
+FrontLegAmplitude = np.pi/6
+FrontLegFrequency = 2.0
+FrontLegPhaseOffset = 2 * np.pi + 0.1
+
+BackLegTargetAngles = np.zeros(1000)
+FrontLegTargetAngles = np.zeros(1000)
+
+for i in range(1000):
+    BackLegTargetAngles[i] = BackLegAmplitude * np.sin(BackLegFrequency * i * np.pi/60 + BackLegPhaseOffset)
+    FrontLegTargetAngles[i] = FrontLegAmplitude * np.sin(FrontLegFrequency * i * np.pi/60 + FrontLegPhaseOffset)
+
 for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    time.sleep(1/120)
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = robotId,
+        jointName = b'Torso_BackLeg',
+        controlMode = p.POSITION_CONTROL,
+        targetPosition = BackLegTargetAngles[i],
+        maxForce = 500)
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = robotId,
+        jointName = b'Torso_FrontLeg',
+        controlMode = p.POSITION_CONTROL,
+        targetPosition = FrontLegTargetAngles[i],
+        maxForce = 500)
+    time.sleep(1/240)
 
 save_path = os.path.join(os.path.dirname(__file__), "data", "back_leg_sensor_values.npy")
 np.save(save_path, backLegSensorValues)
