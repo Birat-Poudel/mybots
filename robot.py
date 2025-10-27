5dec2e1a3f8355ef7c0a6b6bb0c82a8d9191e525
Birat Poudel <biratpoudel@lftechnology.com>
2025-10-27 19:00:51 +0545

    Refactor fitness calculation in ROBOT class to use base position instead of link state

--- robot.py
+++ robot.py
@@ -50,10 +50,10 @@ class ROBOT:
         # self.nn.Print()
     
     def Get_Fitness(self):
-        stateOfLinkZero = p.getLinkState(self.robotId, 0)
-        positionOfLinkZero = stateOfLinkZero[0]
-        xCoordinateOfLinkZero = positionOfLinkZero[0]
+        basePositionAndOrientation = p.getBasePositionAndOrientation(self.robotId)
+        basePosition = basePositionAndOrientation[0]
+        xPosition = basePosition[0]
         f = open("tmp" + str(self.myID) + ".txt", "w")
-        f.write(str(xCoordinateOfLinkZero))
+        f.write(str(xPosition))
         f.close()
         os.system("mv tmp" + str(self.myID) + ".txt fitness" + str(self.myID) + ".txt")
